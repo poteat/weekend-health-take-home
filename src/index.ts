617e7fb00d76f1686b6f1f8bb562765352d932e9
poteat <poteat@users.noreply.github.com>
2024-06-06 16:11:36 -0700

    feat: refactor and elide lodash

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
-import { countBy, every } from "lodash";
+// import { countBy, every } from "lodash"
+
+const countBy = (s: string): Record<string, number> =>
+  [...s].reduce((obj, c) => ({ ...obj, [c]: obj[c] ?? 0 + 1 }), {});
+
+const every = <T>(obj: Record<string, T>, f: (x: T, key: string) => boolean) =>
+  Object.entries(obj).every(([key, value]) => f(value, key));
 
 /**
  * Verify whether or not string `b` can be formed of a rearrangement of string
- * `a`.
+ * `a`'s corpus (count of each letter)
  *
  * Equivalent to whether for each letter in `b`, there exist enough copies of
- * that letter in `a`.
+ * that letter in `a`'s corpus.
  *
- * @param a String which should be rearranged to form `b`.
+ * @param corpus Corpus of string which should be rearranged to form `b`.
  * @param b String which should be created from `a`.
  *
- * @returns Whether `b` can be formed from `a`.
+ * @returns Whether `b` can be formed from `a`'s char count corpus.
  */
-const canBeRearranged = (a: string) => {
-  const corpus = countBy(a);
-  return (b: string) =>
-    every(countBy(b), (amt, key) => (corpus[key] ?? 0) >= amt);
-};
+const canBeRearranged = (corpus: Record<string, number>, b: string) =>
+  every(countBy(b), (amt, key) => (corpus[key] ?? 0) >= amt);
 
 /**
  * Filter elements of `dict` such that those who can be formed from
@@ -28,4 +31,4 @@ const canBeRearranged = (a: string) => {
  * @returns Elements of `dict` which can be formed from `input`.
  */
 export const findWords = (input: string, dict: string[]) =>
-  dict.filter(canBeRearranged(input));
+  dict.filter((x) => canBeRearranged(countBy(input), x));
